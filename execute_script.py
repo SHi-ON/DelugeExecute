ac96dc0b4fa54ea864a135811471bf106e7c6cb3
SHi-ON <7shayan7@gmail.com>
2021-02-14 14:49:31 -0500

    Experimenting

--- execute_script.py
+++ execute_script.py
@@ -1,6 +1,36 @@
 #!/usr/bin/env python
-from sys import argv
+import sys
+import os
+import time
+#import logging
+
+
+import syslog as sl
 from syslog import syslog
+#logging.basicConfig(filename='example.log', level=logging.DEBUG)
+
+sl.openlog(logoption=sl.LOG_DEBUG)
 syslog('deluge test: the script started running')
-for arg in argv[1:]:
+
+for arg in sys.argv[1:]:
     syslog(arg)
+
+arg_id = sys.argv[1]
+arg_name = sys.argv[2]
+arg_path = sys.argv[3]
+
+syslog('here is path: ' + arg_path)
+
+arg_name = arg_name.strip()
+arg_path = arg_path.strip()
+
+#os.system('su centos')
+#syslog('user id: ' + str(os.getuid()))
+
+os.system('mkdir ' + arg_path + '/gg')
+os.system('rsync -arvzh {0}/{1} shi-on@68.129.239.249:/Users/shi-on/Downloads/d'.format(arg_path, arg_name))
+
+syslog('finished uploading to the local machine')
+
+sl.closelog()
+
