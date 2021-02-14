7d7c222783145b3c3b5234b778395e0e8b0823ba
Shayan Amani <7shayan7@gmail.com>
2021-02-14 15:20:53 -0500

    More formatting

--- sentry.py
+++ sentry.py
@@ -5,19 +5,19 @@ from watchdog.observers import Observer
 
 
 def on_created(event):
-    print(f"hey, {event.src_path} has been created!")
+    print(f"hey, {event.src_path} has been created!\n")
 
 
 def on_deleted(event):
-    print(f"what the f**k! Someone deleted {event.src_path}!")
+    print(f"what the f**k! Someone deleted {event.src_path}!\n")
 
 
 def on_modified(event):
-    print(f"hey buddy, {event.src_path} has been modified")
+    print(f"hey buddy, {event.src_path} has been modified\n")
 
 
 def on_moved(event):
-    print(f"ok ok ok, someone moved {event.src_path} to {event.dest_path}")
+    print(f"ok ok ok, someone moved {event.src_path} to {event.dest_path}\n")
 
 
 if __name__ == '__main__':
@@ -36,9 +36,11 @@ if __name__ == '__main__':
                       recursive=True)
 
     observer.start()
+    print('Observer started...')
     try:
         while True:
             time.sleep(1)
     except KeyboardInterrupt:
+        print('\nObserver stopped\n')
         observer.stop()
         observer.join()
