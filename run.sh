00d98ec0a34c81322dcca948395666749a535a34
SHi-ON <7shayan7@gmail.com>
2021-05-04 04:34:31 +0000

    Bypass run instruction in case there is a running instance

--- run.sh
+++ run.sh
@@ -1,1 +1,10 @@
-nohup nice bash -c "/home/ubuntu/miniconda3/envs/Ni9er/bin/python sentry.py" &
+#!/bin/sh
+
+if [ `pgrep -f sentry.py` ];
+then
+    echo "### Sentry is already running!!"
+    exit 1
+else
+    nohup nice bash -c "/home/ubuntu/miniconda3/envs/Ni9er/bin/python sentry.py" &
+    exit 0
+fi
