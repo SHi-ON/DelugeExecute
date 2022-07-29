1b8c85ad6926174e7ed541927302fcc71cf41ebd
Shayan Amani <7shayan7@gmail.com>
2022-07-28 21:23:40 -0400

    Reorg run instructions

--- run.sh
+++ run.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
 
-if [ `pgrep -f sentry.py` ];
-then
-    echo "### Sentry is already running!!"
-    exit 1
+if [ $(pgrep -f sentry.py) ]; then
+  echo "### Sentry is already running!!"
+  exit 1
 else
-    nohup nice bash -c "/home/ubuntu/miniconda3/envs/Ni9er/bin/python sentry.py" &
-    exit 0
+  nohup nice bash -c "/home/ubuntu/miniconda3/envs/WebDev9/bin/python sentry.py" &
+  exit 0
 fi
