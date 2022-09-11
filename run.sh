eea104308994316c13d81ae431fc574d0b9b325b
Shayan Amani <7shayan7@gmail.com>
2022-09-11 12:28:44 -0400

    Path from HOME

--- run.sh
+++ run.sh
@@ -4,6 +4,6 @@ if [ $(pgrep -f sentry.py) ]; then
   echo "### Sentry is already running!!"
   exit 1
 else
-  nohup nice bash -c "/home/ubuntu/miniconda3/envs/WebDev9/bin/python sentry.py" &
+  nohup nice bash -c "~/miniconda3/envs/WebDev9/bin/python sentry.py" &
   exit 0
 fi
