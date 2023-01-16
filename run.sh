28a368c3a7dbc364cf64d35221e5db8c8c1c6fca
Shayan Amani <7shayan7@gmail.com>
2023-01-16 08:33:10 -0500

    Restart Sentry via shell script

--- run.sh
+++ run.sh
@@ -4,13 +4,13 @@ if [ "$(pgrep -f sentry.py)" ]; then
   if [ "$1" = "-r" ]; then
     echo "####### restarting Sentry..."
 
-    echo "### kill Sentry"
+    echo "# kill Sentry"
     kill -9 `pgrep -laf sentry.py | awk '{print $1}'`
     rm nohup.out
 
     sleep 2
 
-    echo "### run Sentry"
+    echo "# run Sentry"
     nohup nice bash -c "~/miniconda3/envs/WebDev9/bin/python sentry.py" &
     exit 0
   else
@@ -18,7 +18,7 @@ if [ "$(pgrep -f sentry.py)" ]; then
     exit 1
   fi
 else
-  echo "### run 1 Sentry"
+  echo "### run Sentry"
   nohup nice bash -c "~/miniconda3/envs/WebDev9/bin/python sentry.py" &
   exit 0
 fi
